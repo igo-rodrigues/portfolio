ead552ed5c69e18eb5ff075db7d942ccf2781323
Igo <igo.rodrigues58@gmail.com>
2025-06-13 15:04:49 -0300

    script.js

--- script.js
+++ script.js
@@ -5,3 +5,9 @@ function navegar(destino) {
     window.location.href = destino;
   }, 300);
 }
+window.addEventListener('DOMContentLoaded', () => {
+  const header = document.querySelector('header');
+  const main = document.querySelector('main');
+  const headerHeight = header.offsetHeight;
+  main.style.paddingTop = `${headerHeight + 40}px`; // 40px extra de margem
+});
